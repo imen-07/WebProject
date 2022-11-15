76e6ffebbcb259e99587499681260aefa817c752
Imen Hentati <imen.hentati@enis.tn>
2022-11-15 23:43:26 +0100

    first commit

---
+++ src/test/java/tn/enis/project/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enis.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
